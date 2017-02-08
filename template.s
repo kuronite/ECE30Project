b7de5eb8b9bc568b405140262e95d8cd0721e3ab
Wilson <wilsonjwtran@gmail.com>
2017-02-07 20:20:31 -0800

    Initial Template

---
+++ template.s
@@ -0,0 +1,92 @@
+# Project ECE 30 Winter 2017
+# Maximum Subarray Sum
+# Student 1 : PID A50001111
+# Lastname, Firstname 
+# Student 2 : PID A50001112
+# Lastname, Firstname. 
+
+.data ## Data declaration section
+## String to be printed:
+string_newline: .asciiz "\n" # newline character
+string_space: .asciiz " " # space character
+string_MaxSubArraySum: .asciiz "Maximum Sub Array Sum is: "
+array1: .word -2, -5, 6, -2, -3, 1, 5, -6
+size: .word 7
+.text ## Assembly language instructions go in text segment
+
+main: ## Start of code section
+li $a1, 0
+la $a2, size 
+lw $a2, 0($a2)
+la $a0, array1
+jal MaximumSubArraySum # MaxSubArraySum(arr,0, size-1);
+move $a0,$v0 #result of MaxSubArraySum is stored in $v1 store that as argument of print sum
+jal printsum
+li $v0, 10 # terminate program
+syscall
+
+##########################################################
+printsum: # Function to print the contents of the array
+# $a0 = value to be printed
+move $t0, $a0
+	
+# print newline character
+la $a0, string_newline
+li $v0, 4
+syscall
+    
+la $a0,  string_MaxSubArraySum
+li $v0, 4
+syscall
+	
+# print the integer at the address $a0
+li $v0, 1
+move $a0,$t0
+syscall
+jr $ra  # Return back
+
+##########################################################
+MaxSumBoundary:
+#	$a0 contains address to arr[].
+#	$a1 contains s 
+#	$a2 contains e
+#	$a3 is the direction (either 0 or 1)
+#	$v0 returns the maximum subarray
+#   Write your code here
+jr   $ra
+
+##########################################################
+MaximumCrossingSum:
+#	$a0 contains arr[]
+#	$a1 contains s
+#	$a2 contains m
+#	$a3 contains e
+#	$v0 returns the maximum sum of arrays that cross the midpoint
+#   Write your code here
+jr $ra
+
+
+##########################################################
+MaximumSubArraySum:
+#	$a0 contains arr[].
+#	$a1 contains s
+#	$a2 contains e
+#   Write your code here
+jr $ra
+
+##########################################################
+FindMax2:
+#	$a1 holds the first number.
+#	$a2 holds the second number.
+#	$v0 contains the maximum between the 2 input numbers.
+#   Write your code here
+jr $ra
+
+FindMax3:
+##########################################################
+#	$a1 holds the first number.
+#	$a2 holds the second number.
+#	$a3 holds the third number.
+#	$v0 contains the maximum among the 3 numbers
+#   Write your code here
+jr $ra
